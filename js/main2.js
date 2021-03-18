4a40cac13814e3409dde006e8f7e43adbd46faf8
mkrivoseina <78380499+mkrivoseina@users.noreply.github.com>
2021-03-18 19:28:43 +0200

    first 3 tasks

---
+++ js/main2.js
@@ -0,0 +1,10 @@
+console.log('Natural numbers are:');
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+console.log('-----------------')
+let sum = 0;
+for (let i = 0; i <= 10; i++)
+sum += i;  {
+    console.log('The sum of first 10 natural numbers:', sum);
+}
---
+++ js/main3.js
@@ -0,0 +1,8 @@
+console.log('The natural numbers up to 7th terms are:')
+const num = 7;
+let sum1 = 0;
+for (let i = 1; i <= num; i++) {
+console.log(i);
+sum1 = sum1 + i;
+}
+console.log('The sum of the natural numbers is:', sum1);
