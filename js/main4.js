57483a84412dd91b740fcd056a5d8d57af40d93f
mkrivoseina <78380499+mkrivoseina@users.noreply.github.com>
2021-03-18 20:35:39 +0200

    4-6

---
+++ js/main4.js
@@ -0,0 +1,6 @@
+console.log('The perfect numbers between 1 to 500 are:')
+for (let i = 0; i <= 500; i++) {
+    if( i === 6 || i === 28 || i === 496 ){
+     console.log(i);
+    }
+}
---
+++ js/main5.js
@@ -0,0 +1,8 @@
+const num = 13;
+if( num % 2 === 0 || num % 3 === 0) {
+       console.log(num, "is NOT prime number");
+} else {
+       console.log( num, "is prime number");
+}
+
+
