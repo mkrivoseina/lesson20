431d2bfe175b425b6f122fb03e77edbe0a744cff
mkrivoseina <78380499+mkrivoseina@users.noreply.github.com>
2021-03-18 21:25:55 +0200

    Update main6.js

--- js/main6.js
+++ js/main6.js
@@ -1,13 +1,17 @@
+let array = [];
 
 console.log('The prime numbers between 1 and 100 are:')
-for (num = 0; num < 101; num++){
+for (num = 2; num < 101; num++){
 let prime = true; 
     for (let i = 2; i < num; i++) {
     if (num % i === 0) {
         prime = false;
 }
-    if (prime === true) {
-        console.log(num)
     }
+    if (prime === true) {
+        console.log(num);
+        console.log(array.push(num));
     }
 }
+
+//not finished, we still need to count the prime numbers (digits)
